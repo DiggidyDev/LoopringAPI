93a56c28a8784a57256ec4c0b8847d786c188f05
Diggy <valentinewilson@gmail.com>
2021-12-29 02:13:20 +0000

    Initial const.py
    The initial endpoints and such that I've added in the first example

---
+++ loopring/util/const.py
@@ -0,0 +1,5 @@
+ENDPOINT_TESTNET_2 = "https://uat2.loopring.io"
+ENDPOINT_TESTNET_3 = "https://uat3.loopring.io"
+ENDPOINT_MAINNET = "https://api3.loopring.io"
+
+RELAYER_CURRENT_TIME = "/api/v3/timestamp"
