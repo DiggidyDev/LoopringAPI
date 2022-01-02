68627b916b222a37693f0282049d5577f8b4bbfe
Diggy <valentinewilson@gmail.com>
2022-01-02 23:50:29 +0000

    Moved const to enums.py
    Better structure to have these consts in `enums.py`

--- loopring/util/const.py
+++
@@ -1,5 +0,0 @@
-ENDPOINT_TESTNET_2 = "https://uat2.loopring.io"
-ENDPOINT_TESTNET_3 = "https://uat3.loopring.io"
-ENDPOINT_MAINNET = "https://api3.loopring.io"
-
-RELAYER_CURRENT_TIME = "/api/v3/timestamp"
