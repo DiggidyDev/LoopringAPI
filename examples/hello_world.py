9b4ac946f8d6bf8db49a7abba29e2625d4501dbd
Diggy <valentinewilson@gmail.com>
2022-01-03 03:39:43 +0000

    Ensure quiet disconnection
    Ensuring the future of closing the client as to avoid Unclosed Session exceptions.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -6,14 +6,18 @@ cfg = {
 
 }
 
-client = loopring.Client("", config=cfg)
+client = loopring.Client(66825, "", config=cfg)  # TODO: endpoint param
+
 
 
 async def main():
-    timestamp = await client.get_relayer_timestamp()
-    print(timestamp)
+    resp = await client.get_next_storage_id(0)
+    print(resp)
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    finally:
+        asyncio.ensure_future(client.close())
