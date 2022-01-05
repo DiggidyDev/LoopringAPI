b4d4ba86b8ee2ff414f4a0b659ed0573343a1752
Diggy <valentinewilson@gmail.com>
2022-01-05 02:28:58 +0000

    Get order details and multiple orders
    Query an account's orders based on certain criteria + fixed some param naming conventions (PEP8) + config bug fix

--- loopring/client.py
+++ loopring/client.py
@@ -1,15 +1,18 @@
 import asyncio
 import json
 from asyncio.events import AbstractEventLoop
-from typing import Union
+from typing import List
 
 import aiohttp
 
 from loopring.errors import *
+from loopring.order import Order
 
 from .util.enums import Endpoints as ENDPOINT
 from .util.enums import Paths as PATH
 from .util.helpers import raise_errors_in, ratelimit
+
+# TODO: In the future, return custom objects instead of dicts
 
 
 class Client:
@@ -31,9 +34,9 @@ class Client:
         if not (cfg.get("endpoint") or endpoint):
             raise InvalidArguments("Missing endpoint from config.")
 
-        self.account_id = config.get("account_id") or account_id
-        self.api_key    = config.get("api_key")    or api_key
-        self.endpoint   = config.get("api_key")    or endpoint
+        self.account_id = cfg.get("account_id") or account_id
+        self.api_key    = cfg.get("api_key")    or api_key
+        self.endpoint   = cfg.get("endpoint")   or endpoint
 
         self._loop: AbstractEventLoop = asyncio.get_event_loop()
         self._session = aiohttp.ClientSession(loop=self._loop)
@@ -64,7 +67,7 @@ class Client:
 
             return content["timestamp"]
 
-    async def get_next_storage_id(self, sellTokenID: int=None) -> dict:
+    async def get_next_storage_id(self, sell_token_id: int=None) -> dict:
         """Get the next storage ID.
 
         Fetches the next order ID for a given sold token. If the need
@@ -74,23 +77,23 @@ class Client:
         Each new order ID can be derived from adding 2 to the last one.
         
         Args:
-            sellTokenID (int): The unique identifier of the token which the user
+            sell_token_id (int): The unique identifier of the token which the user
                 wants to sell in the next order.
         Returns:
-            dict: A dictionary containing the `orderId` and `offchainId`.
+            A :obj:`dict` containing the `orderId` and `offchainId`.
         Raises:
             EmptyAPIKey: No API Key was supplied.
             InvalidAccountID: Supplied account ID was deemed invalid.
             InvalidAPIKey: Supplied API Key was deemed invalid.
             InvalidArguments: Invalid arguments supplied.
-            TypeError: 'sellTokenID' argument supplied was not of type 'int'.
+            TypeError: 'sell_token_id' argument supplied was not of type 'int'.
             UnknownError: Something has gone wrong. Probably out of
                 your control. Unlucky.
             UserNotFound: Didn't find the user from the given account ID.
 
         """
 
-        if not sellTokenID:
+        if not sell_token_id:
             raise InvalidArguments("Missing 'sellTokenID' argument.")
 
         url = self.endpoint + PATH.STORAGE_ID
@@ -99,7 +102,7 @@ class Client:
         }
         params = {
             "accountId": self.account_id,
-            "sellTokenId": sellTokenID
+            "sellTokenId": sell_token_id
         }
 
         async with self._session.get(url, headers=headers, params=params) as r:
@@ -110,4 +113,130 @@ class Client:
             raise_errors_in(content)
 
             return content
+
+    async def get_order_details(self, orderhash: str=None) -> Order:
+        """Get the details of an order based on order hash.
+        
+        Args:
+            orderhash (str): The orderhash belonging to the order you want to
+                find details of.
+        
+        Returns:
+            An :obj:`Order` object.
+
+        Raises:
+            InvalidArguments: Missing the 'orderhash' argument.
+
+        """
+
+        if not orderhash:
+            raise InvalidArguments("Missing 'orderhash' argument.")
+        
+        url = self.endpoint + PATH.ORDER
+        headers = {
+            "X-API-KEY": self.api_key
+        }
+        params = {
+            "accountId": self.account_id,
+            "orderHash": orderhash
+        }
+
+        async with self._session.get(url, headers=headers, params=params) as r:
+            raw_content = await r.read()
+
+            content: dict = json.loads(raw_content.decode())
+
+            raise_errors_in(content)
+
+            order: Order = Order(**content)
+
+            return order
+        
+    async def get_multiple_orders(self, *,
+                                end: int=0,
+                                limit: int=50,
+                                market: str=None,
+                                offset: int=0,
+                                order_types: str=None,
+                                side: str=None,
+                                start: int=None,
+                                status: str=None,
+                                trade_channels: str=None
+                                ) -> List[Order]:
+        """Get a list of orders satisfying certain criteria.
+
+        Note:
+            All arguments are optional. \ 
+            All string-based arguments are case-insensitive. For example,
+            `trade_channels='MIXED'` returns the same results as `trade_channels='mIxEd'`.
+        
+        Args:
+            end (int): The upper bound of an order's creation timestamp,
+                in milliseconds. Defaults to `0`.
+            limit (int): The maximum number of orders to be returned. Defaults
+                to `50`.
+            market (str): The trading pair. Example: `'LRC-ETH'`.
+            offset (int): The offset of orders. Defaults to `0`. \            
+            order_types (str): Types of orders available:
+                `'LIMIT_ORDER'`, `'MAKER_ONLY'`, `'TAKER_ONLY'`, `'AMM'`. 
+            side (str): The type of order made, a `'BUY'` or `'SELL'`.
+            start (int): The lower bound of an order's creation timestamp,
+                in milliseconds. Defaults to `0`.
+            status (str): The order's status:
+                `'PROCESSING'`, `'PROCESSED'`, `'FAILED'`, `'CANCELLED'`, `'CANCELLING'`,
+                `'EXPIRED'`.
+
+                Multiple statuses can be selected:
+                `'CANCELLING, CANCELLED'`
+            trade_channels (str): The channel which said trade was made in:
+                `'ORDER_BOOK'`, `'AMM_POOL'`, `'MIXED'`.
+        
+        Returns:
+            A :obj:`list` of :obj:`Order` objects on a successful query.
+            The returned list could be empty if no orders met the given conditions.
+
+        Raises:
+            EmptyAPIKey: The API Key cannot be empty.
+            EmptyUser: The user ID cannot be empty.
+            InvalidAccountID: The account ID is invalid.
+            InvalidAPIKey: The API Key is invalid.
+            UnknownError: Something out of your control went wrong.
+
+        """
+
+        url = self.endpoint + PATH.ORDERS
+        headers = {
+            "X-API-KEY": self.api_key
+        }
+        params = {
+            "accountId": self.account_id,
+            "end": end,
+            "limit": limit,
+            "market": market,
+            "offset": offset,
+            "orderTypes": order_types,
+            "side": side,
+            "start": start,
+            "status": status,
+            "tradeChannels": trade_channels
+        }
+
+        # Filter out unspecified parameters
+        params = {k: v for k, v in params.items() if v}
+
+        print(params)
+
+        async with self._session.get(url, headers=headers, params=params) as r:
+            raw_content = await r.read()
+
+            content: dict = json.loads(raw_content.decode())
+
+            raise_errors_in(content)
+
+            orders: List[Order] = []
+
+            for order in content["orders"]:
+                orders.append(Order(**order))
+
+            return orders
 
