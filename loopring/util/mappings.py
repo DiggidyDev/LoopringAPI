149d2ae6b9795f02ad87140ef0709de66d66ce0f
Diggy <valentinewilson@gmail.com>
2022-01-05 02:31:07 +0000

    Added mappings for custom order objects
    Also updated error mappings for order-related queries

--- loopring/util/mappings.py
+++ loopring/util/mappings.py
@@ -5,9 +5,25 @@ from .enums import ErrorCodes as ERR
 
 ERROR_MAPPINGS = {
     ERR.EMPTY_API_KEY: EmptyAPIKey,
+    ERR.EMPTY_ORDERHASH: EmptyOrderhash,
+    ERR.EMPTY_USER: EmptyUser,
     ERR.INVALID_ACCOUNT_ID: InvalidAccountID,
     ERR.INVALID_API_KEY: InvalidAPIKey,
     ERR.INVALID_ARGUMENTS: InvalidArguments,
+    ERR.NON_EXISTENT_ORDER: OrderNotFound,
     ERR.UNKNOWN_ERROR: UnknownError,
     ERR.USER_NOT_FOUND: UserNotFound
+}
+
+ORDER_ATTR_MAPPINGS = {
+    "clientOrderId": "client_order_id",
+    "orderType": "order_type",
+    "tradeChannel": "trade_channel"
+}
+
+VOLUME_ATTR_MAPPINGS = {
+    "baseAmount": "base_amount",
+    "baseFilled": "base_filled",
+    "quoteAmount": "quote_amount",
+    "quoteFilled": "quote_filled"
 }
