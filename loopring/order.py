91cb4099b0ce21544ea690a05278ca035cb28c85
Diggy <valentinewilson@gmail.com>
2022-01-06 03:06:08 +0000

    Optional error handling
    Giving the user greater control over exception handling. This resulted in needing to add some checks for incomplete order instantiation attempts (hence __is_error_).
    
    Also docstrings and Mappings refactoring :^)

--- loopring/order.py
+++ loopring/order.py
@@ -1,4 +1,5 @@
-from loopring.util.mappings import ORDER_ATTR_MAPPINGS, VOLUME_ATTR_MAPPINGS
+from typing import Union
+from loopring.util.mappings import Mappings
 
 
 
@@ -26,7 +27,7 @@ class Volume:
     def __init__(self, **data) -> None:
         for k in data:
             if not k.islower():
-                setattr(self, VOLUME_ATTR_MAPPINGS[k], data[k])
+                setattr(self, Mappings.VOLUME_ATTR_MAPPINGS[k], data[k])
                 continue
 
             setattr(self, k, data[k])
@@ -53,9 +54,14 @@ class Order:
     volumes: Volume
 
     def __init__(self, **data) -> None:
+        self.__json = data
+
+        if self.__is_error():
+            return
+
         for k in data:
             if not k.islower():
-                setattr(self, ORDER_ATTR_MAPPINGS[k], data[k])
+                setattr(self, Mappings.ORDER_ATTR_MAPPINGS[k], data[k])
             
             elif k == "validity":
                 setattr(self, k, Validity(**data[k]))
@@ -67,10 +73,39 @@ class Order:
                 setattr(self, k, data[k])
     
     def __repr__(self) -> str:
+        if self.__is_error():
+            return f"<Incomplete order>"
+
         return f"<hash='{self.hash}' id='{self.client_order_id}' " + \
             f"side='{self.side}' market='{self.market}' price='{self.price}' " + \
             f"order_type='{self.order_type}' trade_channel='{self.trade_channel}' " + \
             f"status='{self.status}' validity={self.validity} volumes={self.volumes}>"
     
     def __str__(self) -> str:
+        if self.__is_error():
+            return "Incomplete order object."
+
         return self.hash
+    
+    def __is_error(self):
+        # On an unsuccessful response, the only data in
+        # the dictionary would be "resultInfo" along
+        # with an error code.
+        return len(self.json) < 2
+
+    @property
+    def json(self) -> dict[str, Union[str, dict[str, Union[str, int]]]]:
+        """Returns the original data from which the object was initialised.
+
+        Disabling :obj:`Client.handle_errors` will prevent
+        exceptions from being raised. On a successful response, you will
+        still have an :obj:`~loopring.order.Order` object returned, but in
+        the event that an exception occurs, you'll receive a :py:class:`dict`
+        containing the raw error response data.
+        
+        .. seealso:: :class:`~.util.mappings.Mappings.ERROR_MAPPINGS`
+            in case you have disabled :obj:`Client.handle_errors`
+            and wish to handle the raw error JSON response yourself.
+
+        """
+        return self.__json
