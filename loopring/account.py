80ed4548a62dff039b893e26a07a624c74055541
Diggy <valentinewilson@gmail.com>
2022-01-19 14:00:16 +0000

    `Balance` Model for user balance endpoint

--- loopring/account.py
+++ loopring/account.py
@@ -29,3 +29,43 @@ class Account:
     
     def __str__(self) -> str:
         return self.owner
+
+
+class _PendingBalance:
+
+    deposit: int
+    withdraw: int
+
+    def __init__(self, **data):
+        for k in data.keys():
+            setattr(self, to_snake_case(k), int(data[k]))
+
+    def __repr__(self) -> str:
+        return f"<deposit={self.deposit} withdraw={self.withdraw}>"
+    
+    def __str__(self) -> str:
+        return f"Deposit: {self.deposit}, Withdraw: {self.withdraw}"
+
+
+class Balance:
+
+    account_id: int
+    locked: int
+    pending: _PendingBalance
+    token_id: int
+    total: int
+
+    def __init__(self, **data):
+        for k in data.keys():
+            if k == "pending":
+                self.pending = _PendingBalance(**data[k])
+            else:
+                setattr(self, to_snake_case(k), int(data[k]))
+    
+    def __repr__(self) -> str:
+        return f"<total={self.total} pending={repr(self.pending)} " + \
+            f"token_id={self.token_id} locked={self.locked} " + \
+            f"account_id={self.account_id}>"
+    
+    def __str__(self) -> str:
+        return self.total
