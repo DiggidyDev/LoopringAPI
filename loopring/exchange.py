d4d17ca28ce52f880b6519ab2a29de91d2f87cdc
Diggy <valentinewilson@gmail.com>
2022-01-20 04:07:14 +0000

    `Block` Model support for new endpoint

--- loopring/exchange.py
+++ loopring/exchange.py
@@ -1,7 +1,137 @@
 from datetime import datetime
 from typing import List
 
-from loopring.util.helpers import to_snake_case
+from .util.helpers import auto_repr, to_snake_case
+
+
+class _Order:
+
+    account_id: int
+    amount_b: str
+    amount_s: str
+    fee_bips: int
+    fill_s:  int
+    is_amm: bool
+    nft_data: str
+    storage_id: int
+    taker: str
+    token_b: int
+    token_s: int
+    valid_until: datetime
+
+    def __init__(self, **data):
+        for k in data.keys():
+            if k == "validUntil":
+                ts = datetime.fromtimestamp(data[k])
+                setattr(self, to_snake_case(k), ts)
+                continue
+
+            setattr(self, to_snake_case(k), data[k])
+    
+    def __repr__(self) -> str:
+        return auto_repr(self)
+
+
+class _Token:
+
+    amount: str
+    nft_data: str
+    nft_id: str
+    token_address: str
+    token_id: int
+
+    def __init__(self, **data):
+        for k in data.keys():
+            setattr(self, to_snake_case(k), data[k])
+    
+    def __repr__(self) -> str:
+        return auto_repr(self)
+
+
+class _TxModel:
+
+    account_id: int
+    fee: _Token
+    from_address: str
+    minter: str
+    minter_account_id: int
+    nft_token: _Token
+    nft_type: str
+    nonce: int
+    owner: str
+    order_a: _Order
+    order_b: _Order
+    storage_id: int
+    to_account_address: str
+    to_account_id: int
+    to_address: str
+    to_token: _Token
+    token: _Token
+    tx_type: str
+    valid: bool
+    valid_until: datetime
+
+    def __init__(self, **data):
+        for k in data.keys():
+            if "order" in k:
+                setattr(self, to_snake_case(k), _Order(**data[k]))
+
+            elif k in ("fee", "nftToken", "toToken", "token"):
+                setattr(self, to_snake_case(k), _Token(**data[k]))
+
+            elif k == "validUntil":
+                ts = datetime.fromtimestamp(data[k])
+                setattr(self, to_snake_case(k), ts)
+
+            else:
+                setattr(self, to_snake_case(k), data[k])
+    
+    def __repr__(self) -> str:
+        return auto_repr(self)
+
+
+class Block:
+
+    block_id: int
+    block_size: int
+    created_at: datetime
+    exchange: str
+    status: str
+    transactions: List[_TxModel]
+    tx_hash: str
+
+    def __init__(self, **data):
+        for k in data.keys():
+            if k == "transactions":
+                transactions = []
+
+                for t in data[k]:
+                    transactions.append(_TxModel(**t))
+                
+                setattr(self, to_snake_case(k), transactions)
+            
+            elif k == "createdAt":
+                ts = datetime.fromtimestamp(data[k] / 1000)
+
+                setattr(self, to_snake_case(k), ts)
+
+            else:
+                setattr(self, to_snake_case(k), data[k])
+    
+    def __repr__(self) -> str:
+        attrs = []
+
+        for a in self.__annotations__.keys():
+            if a != "transactions":
+                attrs.append(f"{a}={getattr(self, a)}")
+            else:
+                n = len(getattr(self, a))
+                attrs.append(f"{a}=[{n} Transaction{'s' if n != 1 else ''}]")
+
+        return f"<{' '.join(attrs)}>"
+    
+    def __str__(self) -> str:
+        return f"{self.block_id}"
 
 
 class FeeInfo:
