a33841e7e44f2c103976eb91f369932a1e838b6e
Diggy <valentinewilson@gmail.com>
2022-01-22 23:05:31 +0000

    Update main package imports

--- loopring/__init__.py
+++ loopring/__init__.py
@@ -1,3 +1,6 @@
-from .client import *
+from .amm import AMMTrade, AMMTransaction, ExitPoolTokens, JoinPoolTokens, Pool, PoolSnapshot
+from .client import Client
 from .errors import *
-from .token import *
+from .exchange import Block, DepositHashData, Exchange, TransactionHashData, TransferHashData, TxModel, WithdrawalHashData
+from .order import CounterFactualInfo, Order, OrderBook, PartialOrder, Transfer
+from .token import Fee, Price, Rate, RateInfo, Token, TokenConfig
